4e2cb43ac4f9dc66948b6b6872c71fc3b68313e5
YUJEONG\kim94 <claudia@claudia.com>
2024-07-10 22:34:25 +0900

    n의 배수, 공배수

---
+++ src/programmers/test/Test16.java
@@ -0,0 +1,15 @@
+package programmers.test;
+
+public class Test16 {
+
+    public int solution(int num, int n) {
+        int answer = 0;
+        if(num % n == 0) {
+            answer = 1;
+        } else {
+        	answer = 0;
+        }
+        return answer;
+    }
+
+}
---
+++ src/programmers/test/Test17.java
@@ -0,0 +1,10 @@
+package programmers.test;
+
+public class Test17 {
+
+    public int solution(int number, int n, int m) {
+        int answer = number % n == 0 && number % m == 0 ? 1 : 0;
+        return answer;
+    }
+
+}
