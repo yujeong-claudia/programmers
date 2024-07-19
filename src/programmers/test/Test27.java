33c474e309b7ba84e5a1ae43596bd08ccc54c517
YUJEONG\kim94 <claudia@claudia.com>
2024-07-19 22:47:34 +0900

    두 수의 나눗셈

---
+++ src/programmers/test/Test27.java
@@ -0,0 +1,9 @@
+package programmers.test;
+
+public class Test27 {
+    public int solution(int num1, int num2) {
+        double result = (double)num1 / (double)num2 * 1000;
+        int answer = (int)result;
+        return answer;
+    }
+}
