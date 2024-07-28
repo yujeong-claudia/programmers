222a5ccbd314d57d267497c610afc2e879e61868
YUJEONG\kim94 <claudia@claudia.com>
2024-07-28 09:44:48 +0900

    아이스 아메리카노

---
+++ src/programmers/test/Test39.java
@@ -0,0 +1,11 @@
+package programmers.test;
+
+public class Test39 {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+        return answer;
+    }
+}
