ad82a054f9d42c2c0b6abb702eb40d1f424a72f5
YUJEONG\kim94 <claudia@claudia.com>
2024-07-30 21:00:56 +0900

    순서쌍의 개수

---
+++ src/programmers/test/Test42.java
@@ -0,0 +1,12 @@
+package programmers.test;
+
+public class Test42 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) 
+            answer++;
+        }
+        return answer;
+    }
+}
