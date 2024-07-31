7498b1e07b56c3807ac97f66bc5a458fbecad8ce
YUJEONG\kim94 <claudia@claudia.com>
2024-07-31 20:55:31 +0900

    개미군단

---
+++ src/programmers/test/Test43.java
@@ -0,0 +1,9 @@
+package programmers.test;
+
+public class Test43 {
+    public int solution(int hp) {
+        int answer = 0;
+        answer = (hp / 5) + ((hp % 5) / 3) + ((hp % 5) % 3);
+        return answer;
+    }
+}
