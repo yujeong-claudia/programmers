0d5f3a434db8a71426453260bd829da1e44303dd
YUJEONG\kim94 <claudia@claudia.com>
2024-08-02 23:19:58 +0900

    짝수 홀수 개수

---
+++ src/programmers/test/Test45.java
@@ -0,0 +1,18 @@
+package programmers.test;
+
+public class Test45 {
+    public int[] solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        int[] answer = {even, odd};
+        return answer;
+    }
+}
