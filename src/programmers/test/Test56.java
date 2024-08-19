7a5c25118974d7c18787518c64bcb8003fdd3819
YUJEONG\kim94 <claudia@claudia.com>
2024-08-19 22:41:39 +0900

    배열 원소의 길이

---
+++ src/programmers/test/Test56.java
@@ -0,0 +1,11 @@
+package programmers.test;
+
+public class Test56 {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i = 0; i < strlist.length; i++){
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}
