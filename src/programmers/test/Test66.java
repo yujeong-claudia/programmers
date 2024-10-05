7e7cb64bb7d489a05c6061a18fc4aa358c45c021
yujeong-claudia <kim94sujung94@gmail.com>
2024-10-05 23:46:17 +0900

    접미사인지 확인하기

---
+++ src/programmers/test/Test66.java
@@ -0,0 +1,8 @@
+package programmers.test;
+
+public class Test66 {
+    public int solution(String my_string, String is_suffix) {
+        
+        return my_string.endsWith(is_suffix) ? 1 : 0;
+    }
+}
