3835ef903b701f5b539b5c953df2edb46df39886
yujeong-claudia <kim94sujung94@gmail.com>
2024-10-08 23:29:37 +0900

    수 조작하기 1

---
+++ src/programmers/test/Test69.java
@@ -0,0 +1,23 @@
+package programmers.test;
+
+public class Test69 {
+    public int solution(int n, String control) {
+        for (char c : control.toCharArray()) {
+            switch (c) {
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1; 
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+            }
+        }
+        return n; 
+    }
+}
