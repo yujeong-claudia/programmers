850e1604ef15a48d430f7510f91ec7bf42213146
yujeong-claudia <kim94sujung94@gmail.com>
2024-10-13 00:04:20 +0900

    조건에 맞게 수열 변환하기 3

---
+++ src/programmers/test/Test73.java
@@ -0,0 +1,18 @@
+package programmers.test;
+
+public class Test73 {
+	public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (k % 2 == 1) {
+                answer[i] = arr[i] * k;
+            } 
+            else {
+                answer[i] = arr[i] + k;
+            }
+        }
+        
+        return answer;
+    }
+}
