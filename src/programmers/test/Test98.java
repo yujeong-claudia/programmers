eaee10a819e01cb1d060dd2f6052c8a1f7db04de
yujeong-claudia <kim94sujung94@gmail.com>
2024-11-15 23:48:15 +0900

    공백으로 구분하기 2

---
+++ src/programmers/test/Test98.java
@@ -0,0 +1,8 @@
+package programmers.test;
+
+public class Test98 {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.trim().split("\\s+");
+        return answer;
+    }
+}
