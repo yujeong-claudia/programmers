cc288ffc66630dfc1590dc34ae889cf8950b5b4d
yujeong-claudia <kim94sujung94@gmail.com>
2024-11-16 23:33:28 +0900

    공백으로 구분하기 1

---
+++ src/programmers/test/Test99.java
@@ -0,0 +1,8 @@
+package programmers.test;
+
+public class Test99 {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split("\\s+");
+        return answer;
+    }
+}
