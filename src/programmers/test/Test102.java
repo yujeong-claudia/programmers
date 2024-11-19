3c1f6bc1596776d59761bc048f293211c28432c9
yujeong-claudia <kim94sujung94@gmail.com>
2024-11-19 20:36:37 +0900

    배열에서 문자열 대소문자 변환하기

---
+++ src/programmers/test/Test102.java
@@ -0,0 +1,15 @@
+package programmers.test;
+
+public class Test102 {
+    public String[] solution(String[] strArr) {
+        for(int i = 0; i < strArr.length; i++){
+            if(i % 2 == 0){
+                strArr[i] = strArr[i].toLowerCase();
+            } else {
+                strArr[i] = strArr[i].toUpperCase();
+            }
+           
+        }
+         return strArr;
+    }
+}
