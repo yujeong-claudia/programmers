2442b685d895e28498c775d7238ca8eeb7e889e7
yujeong-claudia <kim94sujung94@gmail.com>
2024-11-25 15:43:00 +0900

    5명씩

---
+++ src/programmers/test/Test108.java
@@ -0,0 +1,15 @@
+package programmers.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test108 {
+	    public String[] solution(String[] names) {
+	        List<String> result = new ArrayList<>();
+	        
+	        for(int i = 0; i < names.length; i += 5){
+	            result.add(names[i]);
+	        }
+	        return result.toArray(new String[0]);
+	    }
+}
