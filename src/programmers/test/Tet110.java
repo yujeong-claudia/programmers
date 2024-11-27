e65439f0031a047cdaa42731db2e0399ca6ba186
yujeong-claudia <kim94sujung94@gmail.com>
2024-11-27 22:16:53 +0900

    n 번째 원소까지

---
+++ src/programmers/test/Tet110.java
@@ -0,0 +1,9 @@
+package programmers.test;
+
+import java.util.Arrays;
+
+public class Tet110 {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, 0, n);
+    }
+}
