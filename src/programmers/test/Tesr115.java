f4fe18708c6466d24fc9c495a321d2e44a47e666
yujeong-claudia <kim94sujung94@gmail.com>
2024-12-03 21:39:28 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/test/Tesr115.java
@@ -0,0 +1,11 @@
+package programmers.test;
+
+public class Tesr115 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for(int i = 0; i < n; i++){
+            answer[i] = (long) x * (i + 1);
+        }
+        return answer;
+    }
+}
