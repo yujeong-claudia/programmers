31a3b9bc411899dfd21ada42b9450e901890ee2b
yujeong-claudia <kim94sujung94@gmail.com>
2024-12-15 23:17:08 +0900

    서울에서 김서방 찾기

---
+++ src/programmers/test/Test125.java
@@ -0,0 +1,16 @@
+package programmers.test;
+
+public class Test125 {
+    public String solution(String[] seoul) {
+        String answer = "";
+        
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i +"에 있다";
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
