b191e301fd8c41290f8fa5f4a1249233a47b9d42
yujeong-claudia <kim94sujung94@gmail.com>
2025-01-07 14:13:43 +0900

    같은 숫자는 싫어

---
+++ src/programmers/test/Test140.java
@@ -0,0 +1,20 @@
+package programmers.test;
+
+public class Test140 {
+    public int[] solution(int []arr) {
+        int[] answer = new int[arr.length];
+        int prev = -1;
+        int index = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != prev){
+                answer[index] = arr[i];
+                index++;
+                prev = arr[i];
+            }
+        }
+        
+        return Arrays.copyOf(answer, index);
+
+    }
+}
